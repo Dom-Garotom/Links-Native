7072045bde6878e82bad205636cd49f269e070f7
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-11-02 06:44:49 -0300

    feat: criando o storage da aplicação

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,46 +1,74 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "./styles";
+import "react-native-get-random-values"
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { styles } from "./styles";
 import ListCategory from "@/components/molecula/listCategory";
 import { router } from "expo-router";
 import InputDefault from "@/components/atomo/input";
 import ButtonDefault from "@/components/atomo/buttonDefault";
 import { useState } from "react";
+import { linkStorage } from "@/storage/link-Storage";
+import { v4 as uuidv4 } from 'uuid';
 
 
 export default function AddPage() {
   const [name, setName] = useState('')
   const [url, setUrl] = useState('')
-  const [category, setCategory] =  useState('' )
+  const [category, setCategory] = useState('')
 
-  const handleSubmit = () =>{
-    console.log({
-      name,
-      url,
-      category
-    })
+  const handleSubmit = async () => {
+    try {
+      if (!category.trim()) {
+        return Alert.alert("Categoria", "Você precisa informar a categoria do link!")
+      }
+
+      if (!name.trim()) {
+        return Alert.alert("Nome", "Você precisa informar o nome do link!")
+      }
+
+      if (!url.trim()) {
+        return Alert.alert("Url", "Você precisa informar a URL do link!")
+      }
+
+      const random = uuidv4();
+
+      await linkStorage.save({
+        id: random,
+        name,
+        url,
+        category
+      })
+
+      const data = await linkStorage.get();
+      console.log(data);
+
+    } catch (error) {
+      Alert.alert("Error", "Infelizmente não foi possivel adicionar o link")
+      console.log(error)
+    }
+
   }
 
   return (
     <View style={styles.container}>
 
-        <View style={styles.header}>
-            <TouchableOpacity onPress={() => router.navigate("/")}>
-                <MaterialIcons name="arrow-back-ios-new" color={"#fff"} size={20} />
-            </TouchableOpacity>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.navigate("/")}>
+          <MaterialIcons name="arrow-back-ios-new" color={"#fff"} size={20} />
+        </TouchableOpacity>
 
-            <Text style={styles.text}>Novo</Text>
-        </View>
-        
-        <Text style={styles.label}>Selcione uma categoria</Text>
+        <Text style={styles.text}>Novo</Text>
+      </View>
 
-        <ListCategory onChange={setCategory} isSelected={category}/>
+      <Text style={styles.label}>Selcione uma categoria</Text>
 
-        <View style={styles.form}>
-          <InputDefault placeholder="Name"  onChangeText={setName}/>
-          <InputDefault placeholder="URL" onChangeText={setUrl} />
-          <ButtonDefault content="adicionar" onPress={handleSubmit} />
-        </View>
+      <ListCategory onChange={setCategory} isSelected={category} />
+
+      <View style={styles.form}>
+        <InputDefault placeholder="Name" onChangeText={(text) => setName(text)} />
+        <InputDefault placeholder="URL" onChangeText={(text) => setUrl(text)} />
+        <ButtonDefault content="adicionar" onPress={handleSubmit} />
+      </View>
     </View>
   )
 }
---
+++ src/storage/link-Storage.ts
@@ -0,0 +1,34 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const key_Link_Storage = "link-Storage";
+
+type LinkStorage = {
+    id : string,
+    name: string,
+    url : string,
+    category : string
+}
+
+
+async function get (): Promise<LinkStorage[]>  {
+    const storage = await AsyncStorage.getItem(key_Link_Storage);
+    const response = storage ? JSON.parse(storage) : [];
+
+    return response
+}
+
+async function save ( newLink : LinkStorage){
+    try{
+        const storage = await get();
+        const update = JSON.stringify([...storage , newLink]);
+
+
+        AsyncStorage.setItem(key_Link_Storage , update);
+    
+    } catch (err) {
+        throw err;
+    }
+}
+
+
+export const linkStorage = { get , save}
